213bf947b29ca3871632c325d97149f150809f3f
王阳 <1070942929@qq.com>
2017-12-08 13:18:02 +0800

    可以获取专辑内容，网页的内容是动态生成的。

--- get_songid.py
+++ get_songid.py
@@ -1,17 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
-url = 'https://music.163.com/#/album?id=36709029'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+session = requests.Session()
+url = 'https://music.163.com/album?id=36709029'
+s = session.get(url)
+soup = BeautifulSoup(s.text, 'html.parser')
 
-song_table = soup.find_all(class_='m-table')
-print(song_table)
+getJSON = soup.select("#song-list-pre-cache textarea")[0].contents[0]
 
-# song_table = soup.select('.ply').get_text()
-# print(song_table)
+j = json.loads(getJSON)
+
+# 查看JSON文件。
+# print(j)
+
+for i in j:
+    print(i['name'])
+    print(i['id'])
+    print(i)
 #
-# for song_item in song_items:
-#     song_id = song_item.data - res - id
-#     print(song_id)
+# # print(soup)
+#
+#
+# song_table = soup.find_all(class_='m-table')
+# # print(song_table)
+#
+# song_ply = soup.select('.ply').get_text()
+# print(song_ply)
+# #
+# # for song_item in song_items:
+# #     song_id = song_item.data - res - id
+# #     print(song_id)
