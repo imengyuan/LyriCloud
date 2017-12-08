c3330207ef2a66407400cf947e9e8fd8cd6b0760
王阳 <1070942929@qq.com>
2017-12-08 13:28:21 +0800

    添加打印id和name的示例。

--- get_songid.py
+++ get_songid.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 import json
 
 session = requests.Session()
@@ -10,24 +9,9 @@ soup = BeautifulSoup(s.text, 'html.parser')
 
 getJSON = soup.select("#song-list-pre-cache textarea")[0].contents[0]
 
-j = json.loads(getJSON)
-
-# 查看JSON文件。
-# print(j)
-
-model = j  # 数据
+json_data = json.loads(getJSON)
 with open("./json/reputation.json", 'w', encoding='utf-8') as json_file:
-    json.dump(model, json_file, ensure_ascii=False)
+    json.dump(json_data, json_file, ensure_ascii=False)
 
-# # print(soup)
-#
-#
-# song_table = soup.find_all(class_='m-table')
-# # print(song_table)
-#
-# song_ply = soup.select('.ply').get_text()
-# print(song_ply)
-# #
-# # for song_item in song_items:
-# #     song_id = song_item.data - res - id
-# #     print(song_id)
+for item in json_data:
+    print("id = ", item['id'], ", name = ", item['name'])
